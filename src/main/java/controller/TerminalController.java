1988d71e637cf5a0716288bc265f327af8dd9881
ruben <ruben.leibmann@gmx.de>
2020-05-18 17:38:56 +0200

    Removed Terminal navigation

--- src/main/java/controller/TerminalController.java
+++ src/main/java/controller/TerminalController.java
@@ -1,12 +1,9 @@
 package controller;
 
-import entities.Guest;
 import gui.AddGuestView;
 import gui.GuestListView;
 import gui.MainView;
 import repos.GuestList;
-
-import java.util.Scanner;
 
 public class TerminalController {
 
@@ -14,8 +11,6 @@ public class TerminalController {
     public final MainView mainView;
     public GuestListView guestListView; // Non-final -> reassigned when a new guest is added to the list
     public final AddGuestView addGuestView;
-    Scanner scanner = new Scanner(System.in);
-    String command = "menu";
 
     public TerminalController() {
         this.guestList = new GuestList();
@@ -26,40 +21,6 @@ public class TerminalController {
 
     public static void main(String[] args) {
         TerminalController controller = new TerminalController();
-
-        controller.enterCommand();
-
-        while(!controller.command.equals("exit")) {
-            controller.navigate();
-        }
         controller.guestList.save();
-    }
-
-    public void enterCommand() {
-        command = scanner.nextLine();
-    }
-
-    private void navigate() {
-        switch(command) {
-            case "menu":
-                System.out.println("Dummy Menu");
-                break;
-            case "new":
-                System.out.println("Enter firstname: ");
-                String firstname = scanner.nextLine();
-                System.out.println("Enter lastname: ");
-                String lastname = scanner.nextLine();
-
-                guestList.add(new Guest(firstname, lastname));
-                guestList.save();
-                break;
-            case "list":
-                System.out.println(guestList.toString());
-                break;
-            default:
-                command = "menu";
-                break;
-        }
-        enterCommand();
     }
 }
